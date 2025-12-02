c84f470ce962cfd8b9581b6f237e81cd3a587e1a
ujgec <2052931891@qq.com>
2025-12-02 10:30:17 +0800

    第二次作业

---
+++ homework2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+ones = np.ones((5, 5))
+
+ones[:]=0
+ones[2, :] = 1
+ones[:, 2] = 1
+
+print(ones)
+
+plt.imshow(ones, cmap='gray_r')
+
+plt.show()
